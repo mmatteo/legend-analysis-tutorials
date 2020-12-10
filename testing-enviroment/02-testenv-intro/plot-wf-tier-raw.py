232d0cf0a346f88184e2f68191f8be0fb1ae6156
George Marshall <george.marshall.20@ucl.ac.uk>
2020-12-10 09:45:57 +0000

    added scripts as separate files

---
+++ testing-enviroment/02-testenv-intro/plot-wf-tier-raw.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import pygama.io.lh5 as lh5
+import numpy as np
+
+f_raw = f'tier1.lh5'
+
+sto = lh5.Store()
+tb_data, n_rows = sto.read_object('/raw', f_raw,30)
+
+wfs_all = tb_data['waveform']['values'].nda
+wf0 = wfs_all[0, :]
+ts = np.arange(0, wf0.shape[0], 1)
+plt.plot(ts[:], wf0[:])
+plt.show()
